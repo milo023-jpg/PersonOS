4011651aa1f78c165a4084cc25f0d7e1cdad66da
milo023-jpg <miloar023@gmail.com>
2026-08-17 07:44:59 -0600

    Corrige la compilación del service worker en Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { registerSW } from "virtual:pwa-register";
 import AppRouter from "./routes/AppRouter";
 import AuthBootstrap from "./modules/auth/presentation/components/AuthBootstrap";
 import "@blocknote/core/fonts/inter.css";
 import "@blocknote/mantine/style.css";
 import "./index.css";
-
-registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
